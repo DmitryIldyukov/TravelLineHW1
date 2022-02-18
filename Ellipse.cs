78917494ba4d1354062d61e7726733a0756af728
Dmitry <99485278+DmitryIldyukov@users.noreply.github.com>
2022-02-18 16:49:32 +0300

    Add files via upload

---
+++ Ellipse.cs
@@ -0,0 +1,29 @@
+﻿namespace Travel
+{
+    public class Ellipse
+    {
+        public int MajorAxis { get; set; }
+        public int MinorAxis { get; set; }
+
+        public Ellipse(int majorAxis, int minorAxis)
+        {
+            if (majorAxis <= minorAxis)
+            {
+                throw new ArgumentException("ERROR!");
+            }
+
+            MajorAxis = majorAxis;
+            MinorAxis = minorAxis;
+        }
+
+        public double GetSquare()
+        {
+            return Math.Round(MinorAxis * MajorAxis * Math.PI);
+        }
+
+        public double GetLength()
+        {
+            return Math.Round((MinorAxis + MajorAxis) * Math.PI);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Travel;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int MajorAxis;
+        int MinorAxis;
+        Console.Write("Введите малый радиус: ");
+        MinorAxis = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите больший радиус: ");
+        MajorAxis = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            Ellipse ellipse = new Ellipse(MajorAxis, MinorAxis);
+            double square = ellipse.GetSquare();
+            double lenght = ellipse.GetLength();
+            Console.WriteLine("Площадь эллипса: " + square);
+            Console.WriteLine("Длина окружности эллипса: " + lenght);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
